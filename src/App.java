615188c2fb6a8a5c257652771a3d4e318ade5816
Diego Perez <pg19012@ues.edu.sv>
2023-02-16 12:33:27 -0600

    Commit con el programa terminado

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println("La suma es: " + sumade3(5, 5, 5));
+        coche miCoche = new coche();
+        System.out.println( "El numero de puertas inicial es: " + miCoche.puertas);
+        miCoche.aumentarPuerta();
+        System.out.println( "El numero de puertas es: " + miCoche.puertas);
     }
+
+    public static int sumade3(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+class coche {
+    public int puertas;
+
+    public void aumentarPuerta() {
+        this.puertas++;
+    }
+
+
 }
