e84e8ca869c70db794f57811f8e5950120b9b42d
Diego Perez <pg19012@ues.edu.sv>
2023-02-16 12:37:23 -0600

    Commit final

--- src/App.java
+++ src/App.java
@@ -1,13 +1,15 @@
 
 public class App {
     public static void main(String[] args) {
+
         System.out.println("La suma es: " + sumade3(5, 5, 5));
         coche miCoche = new coche();
-        System.out.println( "El numero de puertas inicial es: " + miCoche.puertas);
+        System.out.println("El numero de puertas inicial es: " + miCoche.puertas);
         miCoche.aumentarPuerta();
-        System.out.println( "El numero de puertas es: " + miCoche.puertas);
+        System.out.println("El numero de puertas es: " + miCoche.puertas);
     }
 
+    // funcion para hacer la suma
     public static int sumade3(int a, int b, int c) {
         return a + b + c;
     }
@@ -16,9 +18,9 @@ public class App {
 class coche {
     public int puertas;
 
+    // metodo para aumentar las puertas
     public void aumentarPuerta() {
         this.puertas++;
     }
-
 
 }
